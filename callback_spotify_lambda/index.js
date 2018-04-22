fbff768ba6fb34729eae34fa4b9c980785e28abe
Nicholas Del Nano <nickdelnano@gmail.com>
2018-04-22 15:29:06 -0400

    Add random registration code to spotify callback lambda

--- callback_spotify_lambda/index.js
+++ callback_spotify_lambda/index.js
@@ -1,4 +1,8 @@
 var request = require('request');
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB();
+
+let USERS_TABLE_NAME = process.env.DYNAMO_USERS_TABLE_NAME;
 
 exports.handler = (event, context, callback) => {
   var client_id = process.env.client_id
@@ -6,7 +10,6 @@ exports.handler = (event, context, callback) => {
   var redirect_uri = process.env.redirect_uri
 
   var code = event["queryStringParameters"]['code']
-  console.log('CODE ' + code)
 
   if (event["queryStringParameters"]['state'] === null) {
     console.error('state mismatch!!!')
@@ -31,13 +34,12 @@ exports.handler = (event, context, callback) => {
         var access_token = body.access_token,
           refresh_token = body.refresh_token;
 
-        console.log(access_token);
-        console.log(refresh_token);
-
         var random_code = generateRandomString(16);
         var responseBody = {
           message: `Use this code to register. Follow the instructions on the Send Songsss Facebook page. Code: ${random_code}`
         };
+
+        insertRandomCodeIntoUsersTable(random_code, access_token, refresh_token)
 
         var response = {
           statusCode: 200,
@@ -65,3 +67,33 @@ var generateRandomString = function(length) {
   }
   return text;
 };
+
+/* 
+ * Since PSID is PK of users table and it must be filled, set random 16 digit code
+ * as PSID. This will be overwritten with the user's PSID when the user
+ * sends the random code to the bot.
+ */
+function insertRandomCodeIntoUsersTable(registration_code, auth_token, refresh_token) {
+  var params = {
+    Item: {
+      "psid": {
+        S: registration_code
+      },
+      "auth_toke": {
+        S: auth_token
+      }, 
+      "refresh_token": {
+        S: refresh_token
+      } 
+    },
+    ReturnConsumedCapacity: "NONE", 
+    TableName: USERS_TABLE_NAME
+  }
+
+  dynamodb.putItem(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+      console.log(`Successful insertion of ${registration_code} into users table`);
+    }
+  });
+}
