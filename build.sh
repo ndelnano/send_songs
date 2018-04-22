6521c387d2b4991b96c5a0b2a4684865b0cae47c
Nicholas Del Nano <nickdelnano@gmail.com>
2018-04-21 23:12:07 -0400

    git ignore .zip for TF and create build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-zip -r lambda2.zip lambda2.js node_modules
 zip -r terraform/code/lambda1.zip lambda1/lambda1.js lambda1/node_modules
+zip -r terraform/code/lambda2.zip lambda2/lambda2.js lambda2/node_modules
